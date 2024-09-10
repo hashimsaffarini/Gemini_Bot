2cc4758cc97adf2775744327ee13e92a982744d1
hashimsaffarini <hashimsaffarini044@gmail.com>
2024-09-10 19:49:46 +0300

    Refactor main.dart to ensure WidgetsBinding is initialized before calling runApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:gemeni_bot/features/home/logic/home_cubit.dart';
 import 'package:gemeni_bot/gemini_app.dart';
 
 Future<void> main() async {
+  //? Ensure that the WidgetsBinding is initialized before calling runApp
+
   WidgetsFlutterBinding.ensureInitialized();
   runApp(BlocProvider(
     create: (context) => HomeCubit(),
